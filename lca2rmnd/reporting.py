17fd6fbfc0accfebf97e245b3eeba916bc5a7b37
Alois Dirnaichner <alodi@directbox.com>
2020-02-03 11:59:40 +0100

    Add some docstrings, produce levelized impact for kWhs to have comparable numbers. Add some parameter checks for the ElectricityLCAReporting class.

--- lca2rmnd/reporting.py
+++ lca2rmnd/reporting.py
@@ -10,18 +10,52 @@ import pandas as pd
 
 
 class ElectricityLCAReporting():
-    def __init__(self, years, scenario,
+    """
+    Report LCA scores for the REMIND electricity sector.
+
+    The class assumes that the current brightway project contains
+    all the relevant databases, including the characterization methods.
+
+    :ivar scenario: name of the REMIND scenario, e.g., 'BAU', 'SCP26'.
+    :vartype scenario: str
+    :ivar years: years of the REMIND scenario to consider, between 2005 and 2150
+        (5 year steps for up to 2060, then 10 year steps to 2110,
+        and 20 years for the last two time steps).
+        The corresponding brightway databases have to be part of the current project.
+    :vartype year: array
+    :ivar indicatorgroup: name of the set of indicators to
+        calculate the scores for, defaults to ReCiPe Midpoint (H)
+    :vartype source_db: str
+
+    """
+    def __init__(self, scenario, years,
                  indicatorgroup='ReCiPe Midpoint (H)'):
         self.years = years
         self.scenario = scenario
 
         self.methods = [m for m in bw.methods if m[0] == indicatorgroup]
+        if not self.methods:
+            raise ValueError("No methods found in the current brightway2 project for the following group: {}.".format(indicatorgroup))
+
+        # check for brightway2 databases
+        dbnames = set(["_".join(["ecoinvent", scenario, str(yr)]) for yr in years])
+        missing = dbnames - set(bw.databases)
+        if missing:
+            raise ValueError("The following brightway2 databases are missing: {}".format(missing))
         rdc = RemindDataCollection(self.scenario)
         self.data = rdc.data[rdc.data.Year.isin(self.years)]
-
         self.geo = Geomap()
 
-    def reportSectoralLCA(self):
+    def report_sectoral_LCA(self):
+        """
+        Report sectoral averages for the electricity sector based on the (updated)
+        ecoinvent electricity market groups.
+
+        :return: a dataframe with impacts for the REMIND electricity supply
+            both as regional totals and impacts per kWh.
+        :rtype: pandas.DataFrame
+
+        """
         # low voltage consumers
         low_voltage = [
             "FE|Buildings|Electricity",
@@ -39,7 +73,7 @@ class ElectricityLCAReporting():
         ]
 
         market = "market group for electricity, medium voltage"
-        df_medvolt = self.sum_variables_and_add_scores(market, medium_voltage)
+        df_medvolt = self._sum_variables_and_add_scores(market, medium_voltage)
 
         result = pd.concat([df_lowvolt, df_medvolt])
         result["total_demand"] = result["value"]\
@@ -48,12 +82,17 @@ class ElectricityLCAReporting():
         result["total_score"] = result["total_score"]\
                                 .groupby([result.Region, result.Year, result.method])\
                                 .transform("sum")
-        result["score_ej"] = result["total_score"] / result["total_demand"]
+        result["score_kWh"] = result["total_score"] / (result["total_demand"] * 2.8e11)
 
 
-        return result[["Region", "Year", "method", "total_score", "score_ej"]]
+        return result[["Region", "Year", "method", "total_score", "score_kWh"]]
 
-    def sum_variables_and_add_scores(self, market, variables):
+    def _sum_variables_and_add_scores(self, market, variables):
+        """
+        Sum the variables that belong to the market
+        and calculate the LCA scores for all years,
+        regions and methods.
+        """
         df = self.data[self.data.Variable.isin(variables)]\
                  .groupby(["Region", "Year"])\
                  .sum()
